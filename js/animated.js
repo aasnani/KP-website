71de0ba854fbbc16986d08ed56862cd0c9299879
Armand Asnani <armand@Armands-MacBook-Air.local>
2022-12-17 11:59:03 -0500

    Initial commit

---
+++ js/animated.js
@@ -0,0 +1,33 @@
+$(window).scroll(function () {
+    $('.scroll-move1,.scroll-move2,.scroll-move3').each(function (i, obj) {
+		var athis = $(this);
+		if(isScrolledIntoView(athis) && !isanimated(athis)){
+			 $(this).addClass("scroll-visit");
+		}
+    });
+});
+$(document).ready(function () {
+    $('.scroll-move1,.scroll-move2,.scroll-move3').each(function (i, obj) {
+		var athis = $(this);
+		if(isScrolledIntoView(athis) && !isanimated(athis)){
+			 $(this).addClass("scroll-visit");
+		}
+    });
+});
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height() / 2;
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+function isanimated(obj){
+	if(obj.attr("animated")==="true"){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+//scroll and button animation
---
+++ mail.php
@@ -0,0 +1,88 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+
+  require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+  require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
+
+  if (isset($_POST['name']))
+    $name = $_POST['name'];
+  if (isset($_POST['city']))
+    $city = $_POST['city'];
+  if (isset($_POST['state']))
+    $state = $_POST['state'];
+  if (isset($_POST['zip']))
+    $zip = $_POST['zip'];
+  if (isset($_POST['email']))
+    $email = $_POST['email'];
+  if (isset($_POST['number']))
+    $number = $_POST['number'];
+  if (isset($_POST['address']))
+    $address = $_POST['address'];
+  if (isset($_POST['comment']))
+    $comment = $_POST['comment'];
+  if ($name === '') {
+    echo "Name cannot be empty.";
+    die();
+  }
+  if ($email === '') {
+    echo "Email cannot be empty.";
+    die();
+  } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "Email format invalids.";
+      die();
+    }
+  }
+  if ($comment === '') {
+    echo "Message cannot be empty.";
+    die();
+  }
+
+  // error_reporting(-1);
+  // ini_set('display_errors', 'On');
+  // set_error_handler("var_dump");
+
+
+  $recipient = "mishink7@gmail.com";
+  $subj = "Kaypee Jeweller Contact Us From: $name";
+  $content = "Name: $name \nCity: $city \nState: $state \nZip: $zip \nEmail: $email \nNumber: $number \nAddress: $address \nComment: $comment";
+
+  $mail = new PHPMailer();
+
+  $mail->isSMTP();
+
+  $mail->Host = 'kaypeejewelers.com';
+
+  $mail->Port = 465;
+
+  $mail->SMTPAuth = true;
+
+  $mail->SMTPSecure = ssl;
+
+  $mail->Username = '_mainaccount@kaypeejewelers.com';
+
+  $mail->Password = 'sbyQyR8zYsKv';
+
+  $mail->addAddress($recipient);
+
+  $mail->setFrom('_mainaccount@kaypeejewelers.com');
+
+  $mail->Subject = $subj;
+
+  $mail->Body = $content;
+
+  if (!$mail->send()) {
+    $msg .= 'Mailer Error: ' . $mail->ErrorInfo;
+  } else {
+    $msg .= 'Message sent!';
+  }
+
+
+  // mail($recipient, $subj, $content, $headers) or die("Error!");
+  // $result = mail($recipient, $subj, $content, $headers);
+  // echo '<script>alert('.$result!')</script>';
+  //print json_encode(array('message' => $msg, 'code' => 1));
+  exit();
+?>
