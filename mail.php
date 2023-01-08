fc16207a02291e516661f0ab336b3d884720ebe0
Armand Asnani <aasnani@umass.edu>
2023-01-08 15:55:05 -0500

    Fixed contact us form

--- mail.php
+++ mail.php
@@ -22,28 +22,6 @@
     $address = $_POST['address'];
   if (isset($_POST['comment']))
     $comment = $_POST['comment'];
-  // if ($name === '') {
-  //   echo "Name cannot be empty.";
-  //   die();
-  // }
-  // if ($email === '') {
-  //   echo "Email cannot be empty.";
-  //   die();
-  // } else {
-  //   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  //     echo "Email format invalids.";
-  //     die();
-  //   }
-  // }
-  // if ($comment === '') {
-  //   echo "Message cannot be empty.";
-  //   die();
-  // }
-
-  // error_reporting(-1);
-  // ini_set('display_errors', 'On');
-  // set_error_handler("var_dump");
-
 
   $recipient = "armand.asnani@gmail.com";
   $subj = "Kaypee Jeweller Contact Us From: $name";
@@ -79,10 +57,5 @@
     $msg .= 'Message sent!';
   }
 
-
-  // @mail($recipient, $subj, $content, $headers) or die("Error!");
-  // $result = mail($recipient, $subj, $content, $headers);
-  // echo '<script>alert('.$result!')</script>';
-  // print json_encode(array('message' => $msg, 'code' => 1));
   exit();
 ?>
