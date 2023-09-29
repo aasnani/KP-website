d2109cb51b86dd5cefc500489bba7471d1cf8723
Armand Asnani <aacode20@gmail.com>
2023-09-29 00:25:40 -0400

    large server cleanup, refresh token implemented

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-dotenv.config()
-// Load Node modules
 import express from 'express';
 import nodemailer from 'nodemailer';
-
 import path from 'path';
-const __dirname = path.resolve();
 
-global.options = {}
+dotenv.config()
 
-const transporter = nodemailer.createTransport({
+//Definitions
+const __DIRNAME = path.resolve();
+
+const NODEMAILER_EMAIL_SETTINGS = { 
   name: process.env.HOST,
   host: process.env.HOST,
   port: 465,
@@ -18,30 +17,82 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.CONTACT_US_EMAIL,
     pass: process.env.CONTACT_US_PW,
-  },
+  }
+}
+
+const CC_ADD_CONTACT_ENDPOINT = 'https://api.cc.email/v3/contacts/sign_up_form'
+
+const MAIL_LIST_ID = process.env.MAIN_EMAIL_LIST;
+
+const CC_REFRESH_INFO = {
+  url: `https://authz.constantcontact.com/oauth2/default/v1/token?refresh_token=${process.env.REFRESH_TOKEN}&grant_type=refresh_token`,
+  options: {
+    headers: {
+      'Authorization': 'Basic ' + process.env.BASE64_ENCODE,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    method: 'POST'
+  }
+}
+
+let CC_DATA_ACCESS_HEADERS = {
+  'Content-Type': 'application/json',
+}
+
+//Helper functions
+
+const create_contact_json_string = (email) => {
+  let body = {
+    'create_source': 'Contact',
+    'email_address': email,
+    'list_memberships': [MAIL_LIST_ID]
+  };
+
+  return JSON.stringify(body);
+}
+
+const create_nodemailer_mail_object = (name, email, phone, message) => ({
+  from: process.env.CONTACT_US_EMAIL,
+  to: process.env.CONTACT_US_DESTINATION,
+  subject: `Contact Us Form: ${name}`,
+  text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
 });
+
+const refresh_access_token = async () => {
+  const response = await fetch(CC_REFRESH_INFO.url, CC_REFRESH_INFO.options);
+  const response_json = await response.json();
+  
+  CC_DATA_ACCESS_HEADERS['Authorization'] = 'Bearer ' + response_json['access_token'];
+}
+
+const get_options_object = (method, body) => {
+  let headers_clone = Object.assign({}, CC_DATA_ACCESS_HEADERS);
+  let options = {
+    headers: headers_clone,
+    method: method
+  }
+  if (body != undefined) {
+    options.body = body;
+  }
+  return options;
+}
+
+//Set-up
+
+const transporter = nodemailer.createTransport({NODEMAILER_EMAIL_SETTINGS});
 
 transporter.verify(function (error, success) {
   if (error) {
-    console.log(error);
   } else {
-    console.log("Server is ready to take our messages");
   }
 });
 
-const SUBJECT_FORMAT = "Contact Us Form: "
-
-const refresh_options = {
-    headers: {
-        'Authorization': 'Basic ' + process.env.BASE64_ENCODE,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
-    },
-    method: 'POST'
-}
+await refresh_access_token();
 
 // Initialise Express
 let app = express();
+
 // Render static files
 app.use(express.json());
 app.use(express.static('index.html'));
@@ -50,56 +101,27 @@ app.use('/fonts', express.static('fonts'));
 app.use('/images', express.static('images'));
 app.use('/imagesbaup', express.static('imagesbaup'));
 app.use('/js', express.static('js'));
+
+//Routes
+
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__DIRNAME + '/index.html');
 })
-
-function refreshToken() {
-    global.options = {
-        headers: {
-            'Authorization': 'Bearer ' + process.env.ACCESS_TOKEN,
-            'Content-Type': 'application/json',
-        },
-        method: 'POST'
-    };
-    fetch(`https://authz.constantcontact.com/oauth2/default/v1/token?refresh_token=${process.env.REFRESH_TOKEN}&grant_type=refresh_token`, refresh_options).then(response => {
-        let o2 = Object.assign({}, options);
-        // console.log(response);
-        response.json().then(json => {
-            o2.headers['Authorization'] = 'Bearer ' + json['access_token'];
-            console.log("Here");
-            global.options = o2;
-            console.log("ready");
-        });
-    }).catch(err => {
-        console.log(err);
-    })
-}
-
-refreshToken();
 
 app.post('/addToEmailList', (req, res) => {
     let email = req.query.email_address;
-    let body = JSON.stringify({
-        'create_source': 'Contact',
-        'email_address': email,
-        'list_memberships': [process.env.MAIN_EMAIL_LIST]
-    });
-    global.options.body = body;
-
-    console.log(global.options);
-    fetch('https://api.cc.email/v3/contacts/sign_up_form', options).then(api_response => {
-        console.log(api_response);
-        res.json({
-            'status': 'success'
-        });
-    }).catch(error => {
-        console.log(error);
-        res.json({
-            'status': 'fail'
-        });
-    }).finally(() => {
-        options.body = null;
+    let add_contact_body = create_contact_json_string(email);
+    
+    fetch(CC_ADD_CONTACT_ENDPOINT, get_options_object('POST', add_contact_body)).then(async api_response => {
+      if(api_response.status == 401){
+        await refresh_access_token();
+        let response = await fetch(CC_ADD_CONTACT_ENDPOINT, get_options_object('POST', add_contact_body));
+        if(response.status == 200 | response.status == 201) res.status(201).json({'status': 'success'});
+        else res.status(500).json({'status': 'failure'});
+      } 
+      else res.status(201).json({'status': 'success'});
+    }).catch(async error => {
+      res.status(500).json({'status': 'failure'});
     })
 })
 
@@ -109,22 +131,13 @@ app.post('/sendContactUs', (req, res) => {
   let phone = req.body.phone;
   let message = req.body.message;
 
-  const mail = {
-    from: process.env.CONTACT_US_EMAIL,
-    to: process.env.CONTACT_US_DESTINATION,
-    subject: SUBJECT_FORMAT + name,
-    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
-  };
-
-  console.log(mail);
+  const mail = create_nodemailer_mail_object(name, email, phone, message);
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Something went wrong.");
+      res.status(500).json({'status': 'failure'});
     } else {
-      res.status(200).send("Email successfully sent to recipient!");
-      console.log("Success");
+      res.status(200).json({'status': 'success'});;
     }
   });
 })
